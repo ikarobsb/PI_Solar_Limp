5f69e179642364333496f71f3e0abf046e7cf945
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-06-11 11:33:21 -0300

    Arruma componente Geral

--- frontend/src/components/Geral/Geral.jsx
+++ frontend/src/components/Geral/Geral.jsx
@@ -1,34 +1,23 @@
-import React from "react";
-import './Geral.css';
-import Cabecalho from "../Cabecalho/Cabecalho.js";
-import MenuHorizontal from "../MenuHorizontal/MenuHorizontal.js";
-import AreaDados from "../AreaDados/AreaDados.jsx";
-import Rodape from "../Rodape/Rodape.js";
-import Cards from "../Cards/Cards.jsx";
+import React from 'react'
+import './Geral.css'
+import Cabecalho from '../Cabecalho/Cabecalho.js'
+import MenuHorizontal from '../MenuHorizontal/MenuHorizontal.js'
+import AreaDados from '../AreaDados/AreaDados.jsx'
+import Rodape from '../Rodape/Rodape.js'
+import Cards from '../Cards/Cards.jsx'
 
 function Geral() {
   return (
-    <div className="">
-     <div className="row">
-      <div className="col">
-       <Cabecalho/>
-      </div>
-     </div>
-      {/* <MenuHorizontal/> */}
-     <div className="row ">
-      <div className="col">
-         <AreaDados/>
-         </div>
-     </div>
+    <div className="container-flex">
+      <Cabecalho />
 
-      <div className="row my-2">
-        <div className="col">
-        <Cards />
-        </div>
-      </div>
-      <Rodape/>
+      <AreaDados />
+
+      <Cards />
+
+      <Rodape />
     </div>
-  );
+  )
 }
 
-export default Geral;
+export default Geral
