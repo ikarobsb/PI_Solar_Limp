bd11a2770ef97a35ad803c29c9e9d251911ac0d6
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-06-11 11:33:39 -0300

    Adiciona estado à AreaDados

--- frontend/src/components/AreaDados/AreaDados.jsx
+++ frontend/src/components/AreaDados/AreaDados.jsx
@@ -1,67 +1,89 @@
-import React from "react";
-import Input from '../Input';
-import './AreaDados.css';
+import React from 'react'
+import { useState } from 'react'
+import Input from '../Input'
+import Select from '../Select'
 
+import './AreaDados.css'
 
 export default function AreaDados() {
+  const [simulacaoData, setSimulacaoData] = useState({
+    cep: '',
+    valorDaConta: '',
+    distribuidora: '',
+  })
+
+  function handleInput(event) {
+    const { name, value } = event.target
+    setSimulacaoData({ ...simulacaoData, [name]: value })
+  }
+
+  function showInput(name) {
+    const isValidData = !!simulacaoData[name]
+    return isValidData
+  }
   return (
-    
     <div className="AreaDados">
-      
       <div class="flex-container">
-        
         <div class="form-group">
-            <p>Faça nossa simulação e veja o quanto você pode economizar</p>    
-            
+          <p>Faça nossa simulação e veja o quanto você pode economizar</p>
 
-            <Input placeholder="Digite seu CEP" >
-              {
-                {
-                  icon: <i className="fa-solid fa-location-dot"></i>,
-                  button: <button className="btn btn-primary">  <i class="fa-solid fa-arrow-right"></i> </button>
-                }
-              }
+          <Input name="cep" onClick={handleInput} placeholder="Digite seu CEP">
+            {{
+              icon: <i className="fa-solid fa-location-dot"></i>,
+            }}
+          </Input>
+        </div>
+
+        {showInput('cep') && (
+          <div className="form-group show-input-animation">
+            <Input
+              name="valorDaConta"
+              onClick={handleInput}
+              placeholder="Digite o valor de sua conta"
+            >
+              {{
+                icon: <i class="fa-solid fa-dollar-sign"></i>,
+              }}
             </Input>
-           </div>
-        
-           <div className="form-group ">
-                 
-            <div className="input-group">
-             <div className="input-group-addon">
-             <i class="fa-solid fa-dollar-sign"></i> 
-                   
-             </div>
-             <input className="form-control" id="cep" name="cep" type="text" placeholder="Digite o valor de sua conta"/>
-            </div>
-           </div>
-        
-           <div className="input-group">
-            <div className="input-group-addon">
-            <i class="fa-solid fa-bolt"></i>  
-            
-            </div>
-            
-            <select className="form-control">
-                <option>CPFL Paulista</option>
-                <option>Opção 2</option>
-                <option>Opção 3</option>
-                <option>Opção 4</option>
-              </select>
-           </div>
-     
-          <div className="row mt-3">
-            <div className="col"> 
-              <a id= "login" type="button" href="/areaadm" class="btn btn-light btn-sm w-100">Area Administrativa</a>
-            </div>
-            
-            <div className="col">
-              <button type="button" class="btn btn-primary btn-sm w-100">Simular</button>
-            </div>
           </div>
-          </div>
-          
-          
-    </div>
-  );
-}
+        )}
 
+        {showInput('valorDaConta') && (
+          <div className="form-group show-input-animation">
+            <Select
+              options={[
+                { label: 'CPFL Paulista', value: 1 },
+                { label: 'TESSTE', value: 2 },
+              ]}
+              onChange={handleInput}
+              name="distribuidora"
+            >
+              {{
+                icon: <i class="fa-solid fa-bolt"></i>,
+              }}
+            </Select>
+          </div>
+        )}
+
+        <div className="row my-3">
+          <div className="col">
+            <a
+              id="login"
+              type="button"
+              href="/areaadm"
+              class="btn btn-light btn-sm w-100"
+            >
+              Area Administrativa
+            </a>
+          </div>
+
+          <div className="col">
+            <button type="button" class="btn btn-primary btn-sm w-100">
+              Simular
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
