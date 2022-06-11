58facc32156d1a7af927456cfc1e520659e6f0f7
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-06-11 11:32:58 -0300

    Cria componente INput

--- frontend/src/components/Input/index.jsx
+++ frontend/src/components/Input/index.jsx
@@ -1,20 +1,38 @@
+import { useState } from 'react'
 import './style.css'
 
-export default function Input({ children, placeholder, onInput }) {
+export default function Input({ children, placeholder, onInput = () => {}, onClick = () => {}, name }) {
+  const [value, setValue] = useState('')
+
+  function handleInput(event) {
+    const { value } = event.target
+    setValue(value)
+    onInput(value)
+  }
+
+  function handleClick(event) {
+    event.target.value = value
+    onClick(event)
+  }
   return (
     <div className="input-group">
-      <div className="input-group-prepend ">
-        <span className="input-group-addon">{children.icon}</span>
+      <div className="input-group-prepend">
+        <span className="input-group-addon d-flex">
+          <div className="d-flex align-items-center">{children.icon}</div>
+        </span>
       </div>
       <input
-        onInput={onInput}
+        name={name}
+        onInput={handleInput}
         className="form-control custom-input rounded"
         type="text"
         placeholder={placeholder}
       />
 
       <div className="ml-2">
-      {children.button}
+        <button name={name} onClick={handleClick} className="btn btn-primary">
+          <i class="fa-solid fa-arrow-right"></i>
+        </button>
       </div>
     </div>
   )
